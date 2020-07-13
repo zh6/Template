181247efafc4e8ab95ceefc7e71216b2cbe43d54
zh6666 <1144941477@qq.com>
2020-07-13 15:26:43 +0800

    修改token

--- app/src/main/java/com/zh/template/net/interceptor/TokenInterceptor.java
+++ app/src/main/java/com/zh/template/net/interceptor/TokenInterceptor.java
@@ -3,9 +3,13 @@ package com.zh.template.net.interceptor;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.zh.template.base.MyApplication;
+import com.zh.template.net.service.AllService;
+import com.zh.template.net.use.BaseObserver;
 import com.zh.template.utils.SharedPreferenceUtils;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
@@ -17,12 +21,12 @@ public class TokenInterceptor implements Interceptor {
     @Override
     public Response intercept(Chain chain) throws IOException {
         Request oldRequest = chain.request();
-        Response response = null;
-        // 新的请求,添加参数
+        // 添加参数
         Request newRequest = addParam(oldRequest);
-        response = chain.proceed(newRequest);
-        String responseString = response.body().string();
-        JSONObject jsonObject = JSON.parseObject(responseString);
+        //返回
+        Response response = chain.proceed(newRequest);
+        String responseStr = response.body().string();
+        JSONObject jsonObject = JSON.parseObject(responseStr);
         int state = jsonObject.getIntValue("state");
         if (state == 97) {
             // token失效，重新执行请求
@@ -31,14 +35,13 @@ public class TokenInterceptor implements Interceptor {
             response = chain.proceed(newTokenRequest);
         } else {
             // 这里值得注意。由于前面value.bytes()把响应流读完并关闭了，所以这里需要重新生成一个response，否则数据就无法正常解析了
-            response = response.newBuilder().body(ResponseBody.create(response.body().contentType(), responseString)).build();
+            response = response.newBuilder().body(ResponseBody.create(response.body().contentType(), responseStr)).build();
         }
         return response;
     }
 
     /**
      * 添加公共参数
-     *
      * @param oldRequest
      * @return
      */
@@ -48,31 +51,31 @@ public class TokenInterceptor implements Interceptor {
         if (token.isEmpty() || oldRequest.url().toString().contains("login")) {
             return oldRequest;
         }
-        HttpUrl.Builder url = oldRequest.url()
-                .newBuilder()
-                .setEncodedQueryParameter("token", token);
-        Request newRequest = oldRequest.newBuilder()
-//                .method(oldRequest.method(), oldRequest.body())
-                .url(url.build())
-                .build();
-
+        Request newRequest = oldRequest.newBuilder().addHeader("Authorization",token).build();
         return newRequest;
     }
 
     /**
-     * 同步请求方式，获取最新的Token
-     *
-     * @return
+     * 登录接口获取最新的Token
+     * @throws IOException
      */
     private void getNewToken() throws IOException {
-        // 通过一个特定的接口获取新的token，此处要用到同步的retrofit请求
-//        RetrofitService.getInstance().Login(SharedPreferenceUtils.getUserInfo(MyApplication.getAppContext()).get(0), SharedPreferenceUtils.getUserInfo(MyApplication.getAppContext()).get(1))
-//                .doOnNext(res -> {
-//                    JSONObject obj = JSON.parseObject(JSON.toJSONString(res));
-//                    if (obj.getIntValue("state") == 0) {  //表示登录成功
-//                        SharedPreferenceUtils.saveToken(MyApplication.getAppContext(), obj.getString("token"));
-//                    }
-//                }).subscribe();
+        //重新调用登录接口 获取token
+//        Map<String, String> map = new HashMap<>();
+//        map.put("loginName", SharedPreferenceUtils.getUserInfo(MyApplication.getAppContext()).get(0));
+//        map.put("password", SharedPreferenceUtils.getUserInfo(MyApplication.getAppContext()).get(1));
+//        map.put("systemId", "2");
+//        map.put("flag", "0");
+//        map.put("authCode", "O0i9");
+//        map.put("permisson", "no");
+//        AllService.getInstance().Login(map).subscribe(new BaseObserver<JSONObject>() {
+//            @Override
+//            public void onSuccess(JSONObject response) {
+//                if (response.getIntValue("state") == 0) {  //表示登录成功
+//                    SharedPreferenceUtils.saveToken(MyApplication.getAppContext(), response.getString("token"));
+//                }
+//            }
+//        });
     }
 
 }
